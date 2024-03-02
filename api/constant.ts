98a884a03ffd223cd9132166368d1596f2233f43
jonghunpak <fireking5997@gmail.co.kr>
2024-03-03 02:55:48 +0900

    deploy

--- api/constant.ts
+++ api/constant.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = 'http://localhost:8000';
-
+// export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+console.log(BASE_URL);
 export const LENGTH = 20;
